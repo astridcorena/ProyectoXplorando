77f8a70409efe857c78bb7395db3853808ff30d2
Lulimartinu <lulimartinucci@hotmail.com>
2024-02-19 23:41:42 -0300

    empiezoBackend
    Suboestructura basica del backend

---
+++ Xplorando/Xplorando/src/test/java/com/dh/backend/proyectoIntegrador/Xplorando/XplorandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.backend.proyectoIntegrador.Xplorando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XplorandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
